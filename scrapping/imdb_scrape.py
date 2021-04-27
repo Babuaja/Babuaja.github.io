48511dabc5cc5ba8074e58a3a61e004e50d16ff3
arsalfadilah <arsal.fadilah.tif420@polban.ac.id>
2021-04-27 16:22:48 +0700

    add durasi and fixed time scraping

--- scrapping/imdb_scrape.py
+++ scrapping/imdb_scrape.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import urllib.request
 import json
 
-PATH = "/usr/bin/chromedriver"
+PATH = "E:\Arsal\Materi Kuliah\Proyek 1\Website Tugas\Web Statis 1 (K)\Babuaja.github.io\scrapping\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 driver.get("https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc")
 
@@ -19,7 +19,7 @@ while i <= 100:
         if i % 3 == 0:
             driver.execute_script("window.scrollBy(0, 600);")
             
-
+        #image downloader
         #for img in film.find_elements_by_tag_name("img"):
         #    try:
         #        url = img.get_attribute("src")
@@ -31,24 +31,20 @@ while i <= 100:
         
         i=i+1
 
-        data.append({"judul" : film.find_element_by_tag_name("h3").text.split(". ", 1)[1].split(" (", 1)[0],
-        "genre": film.find_element_by_class_name("genre").text, 
-        "tahun" : film.find_element_by_class_name("lister-item-year").text[1:-1], 
-        "rating": film.find_element_by_tag_name("strong").text,
-        "gambar": film.find_element_by_tag_name("img").get_attribute("src"),
-        "waktu_scrape": datetime.now().strftime("%H:%M:%S")})
+        data.append({"judul" : film.find_element_by_tag_name("h3").text.split(". ", 1)[1].split(" (", 1)[0],"genre": film.find_element_by_class_name("genre").text, "tahun" : film.find_element_by_class_name("lister-item-year").text[1:-1], "rating": film.find_element_by_tag_name("strong").text,"gambar": film.find_element_by_tag_name("img").get_attribute("src"),"waktu_scrape": datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "durasi" : film.find_element_by_class_name("runtime").text})
 
     try:
         link = driver.find_element_by_link_text("Next »")
         link.click()
     except:
         print("gagal ngeklik next")
-        break;
+        break
 
 
-f=open("data/imdb.json", 'w')
-json.dump(data, f, indent=4)
-f.close()
+hasilScrap = open("hasilScraping.json", "w")
+fixData = json.dumps(data, indent=4, sort_keys=True)
+hasilScrap.writelines(fixData)
 
+hasilScrap.close()
 driver.close()
 driver.quit()
