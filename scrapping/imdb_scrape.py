162fde7af8644a8484c356fbb427cc9a0a4e33b7
arsalfadilah <arsal.fadilah.tif420@polban.ac.id>
2021-04-27 16:57:29 +0700

    fix image link

--- scrapping/imdb_scrape.py
+++ scrapping/imdb_scrape.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from datetime import datetime
 import urllib.request
 import json
+import time
 
 PATH = "E:\Arsal\Materi Kuliah\Proyek 1\Website Tugas\Web Statis 1 (K)\Babuaja.github.io\scrapping\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -10,11 +11,12 @@ driver.get("https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,d
 i = 1
 
 data = []
-
+time.sleep(10)
 while i <= 100:
     for film in driver.find_elements_by_class_name("mode-advanced"):
+        print("=====================================")
         print(film.text)
-
+        print("=====================================")
 
         if i % 3 == 0:
             driver.execute_script("window.scrollBy(0, 600);")
@@ -31,7 +33,7 @@ while i <= 100:
         
         i=i+1
 
-        data.append({"judul" : film.find_element_by_tag_name("h3").text.split(". ", 1)[1].split(" (", 1)[0],"genre": film.find_element_by_class_name("genre").text, "tahun" : film.find_element_by_class_name("lister-item-year").text[1:-1], "rating": film.find_element_by_tag_name("strong").text,"gambar": film.find_element_by_tag_name("img").get_attribute("src"),"waktu_scrape": datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "durasi" : film.find_element_by_class_name("runtime").text})
+        data.append({"judul" : film.find_element_by_tag_name("h3").text.split(". ", 1)[1].split(" (", 1)[0],"genre": film.find_element_by_class_name("genre").text, "tahun" : film.find_element_by_class_name("lister-item-year").text[1:-1], "rating": film.find_element_by_tag_name("strong").text,"gambar": str(film.find_element_by_tag_name("img").get_attribute("src")),"waktu_scrape": datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "durasi" : film.find_element_by_class_name("runtime").text})
 
     try:
         link = driver.find_element_by_link_text("Next »")
